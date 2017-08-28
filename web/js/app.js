7a46931af5e284e457b48dd50492122516f2a8c0
Brooks Boyd <bro@oks.me>
2017-08-28 16:01:48 -0500

    Calculate average time from last few blocks

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,5 @@
 var Web3 = require('web3');
+var blockTimes = [];
 
 window.addEventListener('load', function() {
   var w;
@@ -40,6 +41,48 @@ function getLatestBlock(web3) {
   return rpcGet(latestBlock, web3);
 }
 
+function getBlockData(web3, blockHeight) {
+  var def = new $.Deferred();
+  web3.eth.getBlock(blockHeight, false, function(err, rs) {
+    if (err) {
+      def.reject(err);
+      return;
+    }
+    def.resolve(rs);
+  });
+  return def.promise();
+}
+
+function getLastFiveBlocks(web3, blockHeight) {
+  var data = [];
+  var def = new $.Deferred();
+  var curHeight = blockHeight;
+  function doFetch() {
+    getBlockData(web3, curHeight).then(function(rs) {
+      data.push(rs);
+      if (data.length < 5) {
+        curHeight--;
+        doFetch();
+      } else {
+        def.resolve(data);
+      }
+    }, function(err) {
+      def.reject(err);
+    });
+  }
+  doFetch();
+  return def.promise();
+}
+
+function parseAverageBlocktime() {
+  var sum = 0;
+  for (var i = 0; i < blockTimes.length-1; i++) {
+    var delta = blockTimes[i].timestamp - blockTimes[i+1].timestamp;
+    sum += delta;
+  }
+  return sum/blockTimes.length;
+}
+
 
 function startApp(web3) {
 
@@ -48,6 +91,11 @@ function startApp(web3) {
     getLatestBlock(web3).then(function(rs) {
       var blockNum = web3.toBigNumber(rs).toNumber();
       $('#block-number').html(blockNum);
+      getLastFiveBlocks(web3, blockNum).then(function(rs) {
+        blockTimes = rs;
+        var avgTime = parseAverageBlocktime();
+        $('#block-time').html(avgTime + ' seconds/block');
+      });
       document.title = blockNum + ' - Ethereum Blockwatch';
       console.log(blockNum);
       setTimeout(doBlockCheck, 30*1000);
