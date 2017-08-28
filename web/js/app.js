49fdb3f5305799ebb7cfd67445f4ae46cf810c73
Brooks Boyd <bro@oks.me>
2017-08-28 16:41:35 -0500

    Add PouchDB, and the ability to add alarms

--- web/js/app.js
+++ web/js/app.js
@@ -1,5 +1,9 @@
 var Web3 = require('web3');
 var blockTimes = [];
+var currentBlockHeight = 0;
+var isUpdatingDisplay = false;
+
+var db = new PouchDB('ethereum_blockwatch');
 
 window.addEventListener('load', function() {
   var w;
@@ -53,14 +57,14 @@ function getBlockData(web3, blockHeight) {
   return def.promise();
 }
 
-function getLastFiveBlocks(web3, blockHeight) {
+function getLastBlocks(web3, blockHeight, num) {
   var data = [];
   var def = new $.Deferred();
   var curHeight = blockHeight;
   function doFetch() {
     getBlockData(web3, curHeight).then(function(rs) {
       data.push(rs);
-      if (data.length < 5) {
+      if (data.length < num) {
         curHeight--;
         doFetch();
       } else {
@@ -83,26 +87,96 @@ function parseAverageBlocktime() {
   return sum/blockTimes.length;
 }
 
+function showExistingAlarms() {
+  if (isUpdatingDisplay) return;
+  isUpdatingDisplay = true;
+  var $container = $('#existing-alarms').empty();
+  db.allDocs({
+    include_docs: true
+  }).then(function(rs) {
+    rs.rows.map(function(data) {
+      var doc = data.doc;
+      var delta = doc.block - currentBlockHeight;
+
+      var classes = [];
+      if (delta > 0) {
+        classes.push('pending');
+      } else {
+        classes.push('past');
+      }
+      var $row = $('<div id="' + doc._id  + '" class="' + classes.join(' ') + '"></div>');
+      $row.append('<div class="block">' + doc.block + '</div>');
+
+      if (delta > 0) {
+        // Alarm hasn't gone off yet.
+        $row.append('<div class="delta">' + delta + ' blocks to go.</div>');
+      } else {
+        // Alarm already happened
+        $row.append('<div class="delta">' + delta*-1 + ' blocks ago.</div>');
+      }
+      var $delete = $('<button class="delete">X</button>');
+      $delete.on('click', function(e) {
+
+      });
+
+      $row.append($delete);
+
+      $container.append($row);
+    });
+    isUpdatingDisplay = false;
+  });
+}
+
 
 function startApp(web3) {
 
   // Start timer for checking block heights
   function doBlockCheck() {
     getLatestBlock(web3).then(function(rs) {
-      var blockNum = web3.toBigNumber(rs).toNumber();
-      $('#block-number').html(blockNum);
-      getLastFiveBlocks(web3, blockNum).then(function(rs) {
+      currentBlockHeight = web3.toBigNumber(rs).toNumber();
+      $('#block-number').html(currentBlockHeight);
+      getLastBlocks(web3, currentBlockHeight, 10).then(function(rs) {
         blockTimes = rs;
         var avgTime = parseAverageBlocktime();
         $('#block-time').html(avgTime + ' seconds/block');
       });
-      document.title = blockNum + ' - Ethereum Blockwatch';
-      console.log(blockNum);
+      document.title = currentBlockHeight + ' - Ethereum Blockwatch';
+      console.log(currentBlockHeight);
+      showExistingAlarms();
       setTimeout(doBlockCheck, 30*1000);
     }, function(err) {
       console.log(err);
     });
   }
   doBlockCheck();
+
+  $('#new-alarm-btn').on('click', function(e) {
+    var blockHeight = parseInt($('#new-alarm-num').val());
+    if (isNaN(blockHeight) || blockHeight <= 0) {
+      alert('Not a valid block height');
+      return;
+    }
+    if (blockHeight < currentBlockHeight) {
+      alert('Alarm occurs in the past!');
+    }
+
+    db.put({
+      _id:'alarm-'+blockHeight,
+      block: blockHeight
+    }, function(err, rs) {
+      if (err) {
+        if (err.status == 409) {
+          alert('Alarm already exists');
+          return;
+        }
+        alert(err);
+        return;
+      }
+      showExistingAlarms();
+    });
+
+    console.log(blockHeight);
+  });
+  showExistingAlarms();
 
 }
